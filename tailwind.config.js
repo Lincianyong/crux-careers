b056fef3c3aaad1bae1c7d762ae9e84bdc904da3
Albert cristian <albertcristiantohalim@gmail.com>
2022-11-12 00:21:04 +0700

    New files added and modifications on home page

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+      extend: {
+        spacing: {
+          '128': '32rem',
+          '196': '12.25rem',
+          '240': '15rem',
+        }
+      }
+    }
+}
