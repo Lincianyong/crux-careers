cd79e3d30b03e22b935eddbb3665a21be0e37eb2
William Hilmy Susatyo <85691385+Whs9801@users.noreply.github.com>
2022-11-18 21:49:54 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+require 'config.php';
+if(!empty($_SESSION["id"])){
+  $id = $_SESSION["id"];
+  $result = mysqli_query($conn, "SELECT * FROM tb_user WHERE id = $id");
+  $row = mysqli_fetch_assoc($result);
+}
+else{
+  header("Location: login.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Index</title>
+  </head>
+  <body>
+    <h1>Welcome <?php echo $row["name"]; ?></h1>
+    <a href="logout.php">Logout</a>
+  </body>
+</html>
